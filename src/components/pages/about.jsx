4b083725d64fd321ea2a32b5db74d9a431b358c1
david <53884867+donghangwu@users.noreply.github.com>
2020-07-15 18:35:37 -0700

    Update about.jsx

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+import './about.css';
 
 export class About extends Component {
     render() {
         return (
-            <React.Fragment>
+            <div className='about'>
                 <h1>About</h1>
                 <p>This is a TodoList app. It is my first react project</p>
-            </React.Fragment>
+                
+                
+            </div>
         )
     }
 }
